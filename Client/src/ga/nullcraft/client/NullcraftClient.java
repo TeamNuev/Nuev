b7313d8b2194531fef751b650be655e85a0a901c
Smart4296 <jjh4296@naver.com>
2018-04-29 14:29:31 +0900

    Added some starting process

--- Client/src/ga/nullcraft/client/NullcraftClient.java
+++ Client/src/ga/nullcraft/client/NullcraftClient.java
@@ -1,17 +1,23 @@
 package ga.nullcraft.client;
 
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 import ga.nullcraft.client.audio.AudioManager;
 import ga.nullcraft.client.graphics.WindowManager;
 import ga.nullcraft.client.local.LocalGameDirectory;
 import ga.nullcraft.client.model.ModelManager;
 import ga.nullcraft.client.storage.TempStorage;
-import ga.nullcraft.global.IGameDirectory;
 import ga.nullcraft.global.mod.ModLoader;
-
-import java.nio.file.Path;
+import joptsimple.ArgumentAcceptingOptionSpec;
+import joptsimple.NonOptionArgumentSpec;
+import joptsimple.OptionParser;
+import joptsimple.OptionSet;
 
 public class NullcraftClient {
-    private static LocalGameDirectory gameDirectory;
+    private LocalGameDirectory gameDirectory;
     private static ModLoader modLoader;
 
     private WindowManager windowManager;
@@ -45,8 +51,30 @@ public class NullcraftClient {
         return tempStorage;
     }
 
-    public static void main(String[] args){
-    	modLoader = new ModLoader(gameDirectory.getModStorage());
+	public static void main(String[] args){
+		String defaultPath = System.getProperty("user.home") + File.separator + "Nuev";
+		
+		//Parsing arguments
+    	OptionParser parser = new OptionParser();
+    	parser.allowsUnrecognizedOptions();
+    	parser.accepts("width");
+    	parser.accepts("height");
+    	parser.accepts("fullscreen");
+    	parser.accepts("userToken");
+    	parser.accepts("gameDir");
+    	ArgumentAcceptingOptionSpec<Integer> width = parser.accepts("width").withOptionalArg().ofType(Integer.class).defaultsTo(800);
+    	ArgumentAcceptingOptionSpec<Integer> height = parser.accepts("height").withOptionalArg().ofType(Integer.class).defaultsTo(500);
+    	ArgumentAcceptingOptionSpec<Boolean> isFullScreen = parser.accepts("fullscreen").withOptionalArg().ofType(Boolean.class).defaultsTo(false);
+    	ArgumentAcceptingOptionSpec<String> userToken = parser.accepts("userToken").withOptionalArg().ofType(String.class);
+    	ArgumentAcceptingOptionSpec<String> gameDir = parser.accepts("gameDir").withOptionalArg().ofType(String.class).defaultsTo(defaultPath);
+    	NonOptionArgumentSpec<String> nonOptions = parser.nonOptions();
+    	OptionSet options = parser.parse(args);
+    	List<String> nonOptionList = options.valuesOf(nonOptions);
+
+    	NullcraftClient client = new NullcraftClient(Paths.get((String)options.valueOf(gameDir)));
+    	NuevWindow testWindow = new NuevWindow((int)options.valueOf(width), (int)options.valueOf(height), (boolean)options.valueOf(isFullScreen));
+    	testWindow.run();
+    	modLoader = new ModLoader(client.gameDirectory.getModStorage());
     	modLoader.loadMods();
     }
 }
